0ba01af896d14edf53f6f3ab1391a755fa97c61c
mahdi <el.mahdi.hdd.com>
2025-11-09 23:57:56 +0200

    TP1: Implémentation du pattern Observer - Horloges et CompteARebours

--- tp-gl-master/launcher/src/main/java/org/emp/gl/core/launcher/App.java
+++ tp-gl-master/launcher/src/main/java/org/emp/gl/core/launcher/App.java
@@ -1,24 +1,37 @@
 package org.emp.gl.core.launcher;
 
-import org.emp.gl.clients.Horloge ;
+import java.util.Random;
+import org.emp.gl.clients.Horloge;
+import org.emp.gl.clients.CompteARebours;
+import org.emp.gl.time.service.impl.DummyTimeServiceImpl;
+import org.emp.gl.timer.service.TimerService;
 
-/**
- * Hello world!
- *
- */
 public class App {
 
     public static void main(String[] args) {
-
-        testDuTimeService();
+        
+        TimerService tm = new DummyTimeServiceImpl();
+        
+        // Plusieurs horloges
+        Horloge horloge1 = new Horloge("Horloge-1", tm);
+        Horloge horloge2 = new Horloge("Horloge-2", tm);
+        Horloge horloge3 = new Horloge("Horloge-3", tm);
+        
+        // Compte à rebours de 5
+        CompteARebours compte5 = new CompteARebours("Compte-5", 5, tm);
+        
+        // 10 comptes à rebours aléatoires (10-20)
+        Random random = new Random();
+        for (int i = 1; i <= 10; i++) {
+            int valeur = 10 + random.nextInt(11);
+            new CompteARebours("Compte-" + i, valeur, tm);
+        }
     }
 
-    private static void testDuTimeService() {
-        Horloge horloge = new Horloge("Num 1") ;
-    }
-
+    
     public static void clearScreen() {
         System.out.print("\033[H\033[2J");
         System.out.flush();
     }
+
 }
